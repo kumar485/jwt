00dbe5e389fe8d57333e9ae72d52d7c34a914697
Kumar Ghimre <kumar485hyd@gmail.com>
2020-08-25 17:06:17 +0530

    updated

--- src/main/java/com/javainuse/controller/HelloWorldController.java
+++ src/main/java/com/javainuse/controller/HelloWorldController.java
@@ -1,16 +1,35 @@
 package com.javainuse.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.javainuse.model.MyUser;
+import com.javainuse.repo.UserRepo;
 
 @RestController
 @CrossOrigin()
 public class HelloWorldController {
+	@Autowired
+	private UserRepo repo;
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@RequestMapping({ "/hello" })
 	public String hello() {
 		return "Hello World";
+	}
+	
+	@PostMapping("/user")
+	public String createUser(@RequestBody MyUser user) {
+		String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+		repo.save(user);
+		return "User Added Successfully";
 	}
 
 }
